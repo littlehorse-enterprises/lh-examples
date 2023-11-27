a6d5b155b801232007bd22b8479b3097422b69dc
Mateo Rojas <mateo@littlehorse.io>
2023-11-27 15:03:36 -0500

    Formats code

--- common-tasks/java-send-email/app/src/main/java/lh/demo/send/email/EmailSender.java
+++ common-tasks/java-send-email/app/src/main/java/lh/demo/send/email/EmailSender.java
@@ -2,7 +2,6 @@ package lh.demo.send.email;
 
 import java.io.IOException;
 import java.util.Optional;
-import java.util.logging.Logger;
 
 import com.sendgrid.Content;
 import com.sendgrid.Email;
@@ -13,24 +12,24 @@ import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 
 import io.littlehorse.sdk.worker.LHTaskMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class is a basic implementation of a Task Worker which uses the SendGrid
- * Java
- * API to send emails.
+ * Java API to send emails.
  */
 public class EmailSender {
 
-    Logger log = Logger.getLogger(EmailSender.class.getName());
-
-    public static final String SEND_EMAIL_TASK = "send-email";
+    private static final Logger log = LoggerFactory.getLogger(EmailSender.class);
+    private static final String SEND_EMAIL_TASK = "send-email";
 
     private SendGrid sendGridClient;
     private String fromEmail;
 
     public EmailSender(Optional<SendGrid> sendgrid, Optional<String> fromEmail) {
         if (sendgrid.isEmpty()) {
-            log.warning("The SendGrid Client object provided was empty; running in 'dummy mode'.");
+            log.warn("The SendGrid Client object provided was empty; running in 'dummy mode'.");
         } else {
             if (fromEmail.isEmpty()) {
                 throw new IllegalArgumentException(
@@ -66,14 +65,13 @@ public class EmailSender {
 
         log.info("Sending email now");
         Response response = sendGridClient.api(request);
-        int code = response.getStatusCode();
+        boolean successfullySentEmail = response.getStatusCode() >= 200 && response.getStatusCode() < 300;
 
-        boolean ok = code >= 200 && code < 300;
-        if (ok) {
+        if (successfullySentEmail) {
             return "Successfully sent email to %s".formatted(toAddress);
-        } else {
-            throw new RuntimeException("Failed to send the email: %s".formatted(response.getBody()));
         }
+
+        throw new RuntimeException("Failed to send the email: %s".formatted(response.getBody()));
     }
 
 }
