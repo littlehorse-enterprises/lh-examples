e46cf2c7dee35f0eaca0844e1e64e7e7b9e69907
Mateo Rojas <mateo@littlehorse.io>
2023-11-30 10:01:06 -0500

    Implements GET it-request by id

--- applications/it-request/rest-api/app/src/main/java/lh/demo/it/request/api/Controller.java
+++ applications/it-request/rest-api/app/src/main/java/lh/demo/it/request/api/Controller.java
@@ -2,11 +2,18 @@ package lh.demo.it.request.api;
 
 import io.littlehorse.sdk.common.LHLibUtil;
 import io.littlehorse.sdk.common.proto.LHPublicApiGrpc;
+import io.littlehorse.sdk.common.proto.ListVariablesRequest;
 import io.littlehorse.sdk.common.proto.RunWfRequest;
+
+import java.util.List;
 import java.util.UUID;
 
+import io.littlehorse.sdk.common.proto.Variable;
+import io.littlehorse.sdk.common.proto.WfRunId;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,5 +44,31 @@ public class Controller {
         client.runWf(runWf);
 
         return new IdResponse(wfRunId);
+    }
+
+    @GetMapping(path = "/{it-request-id}")
+    public ITRequest getITRequest(@PathVariable("it-request-id") String id) {
+
+        List<Variable> variables = client.listVariables(ListVariablesRequest.newBuilder().setWfRunId(WfRunId.newBuilder().setId(id)).build()).getResultsList();
+
+        String status = variables.stream()
+                .filter(variable -> variable.getId().getName().equals("status") && variable.getId().getThreadRunNumber() == 0)
+                .map(variable -> variable.getValue().getStr())
+                .toList()
+                .get(0);
+
+        String requesterEmail = variables.stream()
+                .filter(variable -> variable.getId().getName().equals("requester-email") && variable.getId().getThreadRunNumber() == 0)
+                .map(variable -> variable.getValue().getStr())
+                .toList()
+                .get(0);
+
+        String itemDescription = variables.stream()
+                .filter(variable -> variable.getId().getName().equals("item-description") && variable.getId().getThreadRunNumber() == 0)
+                .map(variable -> variable.getValue().getStr())
+                .toList()
+                .get(0);
+
+        return new ITRequest(id, status, requesterEmail, itemDescription);
     }
 }
---
+++ applications/it-request/rest-api/app/src/main/java/lh/demo/it/request/api/ITRequest.java
@@ -0,0 +1,3 @@
+package lh.demo.it.request.api;
+
+public record ITRequest(String id, String status, String requesterEmail, String itemDescription) { }
