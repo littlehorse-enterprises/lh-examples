609313290070c1a43aac9c68024321591807bd0f
Christian CAicedo <christian@littlehorse.io>
2025-06-12 15:59:46 -0500

    Big bang promo

--- order-resilience/microservices/customer/src/main/java/io/littlehorse/examples/exceptions/OrderBlocked.java
+++ order-resilience/microservices/customer/src/main/java/io/littlehorse/examples/exceptions/OrderBlocked.java
@@ -2,7 +2,6 @@ package io.littlehorse.examples.exceptions;
 
 import io.littlehorse.sdk.common.LHLibUtil;
 import io.littlehorse.sdk.common.exception.LHTaskException;
-import io.littlehorse.sdk.common.proto.VariableValue;
 
 public class OrderBlocked extends LHTaskException {
     public OrderBlocked(String message) {
---
+++ order-resilience/microservices/promo/src/main/java/io/littlehorse/examples/MyEntityResource.java
@@ -0,0 +1,6 @@
+package io.littlehorse.examples;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+}
