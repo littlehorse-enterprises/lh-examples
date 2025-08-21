77b80cba7c57ffb18694c190e8650b3687d013d6
Adrian Steinsleger <asteins@gmail.com>
2025-08-21 15:23:05 -0500

    Remove theme provider and unused carousel dep

--- demos/it-request/webapp/src/app/layout.tsx
+++ demos/it-request/webapp/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "tailwindcss/tailwind.css";
-import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
 	title: {
@@ -33,14 +32,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en" className={cn("h-screen w-screen", inter.className)}>
+		<html lang="en" className={cn("h-screen w-screen dark", inter.className)}>
 			<body className="h-full w-full min-h-screen bg-black text-white">
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="system"
-					enableSystem
-					disableTransitionOnChange
-				/>  {/* TODO: fix (boilerplate issue with dark mode) */}
 				{children}
 			</body>
 		</html>
